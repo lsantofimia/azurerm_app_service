69f5f8250392a2bb4b8a6818df09f50531ba4776
lsantofimia <lsantofimia@gmail.com>
2024-09-13 11:08:37 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
 provider "azurerm" {
-  features = {}
+  features {}
 }
 
 resource "azurerm_resource_group" "example" {
